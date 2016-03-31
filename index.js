b0b44122a0ad34fa9df803b4c5162ee802dbe932
Wreulicke <masaya2dies@gmail.com>
2016-04-01 07:10:17 +0900

    message position changed

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var io = require('socket.io')(http);
 var path = require('path')
 var fs = require('fs')
 require("./routing/router")(__dirname, app)
-var watch = require('chokidar').watch(".", {persistent: true});
+var watch = require('chokidar').watch(".", {
+  ignored: /(\.git)|(bower_components)|(node_modules)|(\.gitignore)/,
+  persistent: true
+});
 
 watch.on('change', function (path) {
   io.emit('server message', "change file: " + path);
@@ -17,12 +20,13 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     io.emit('chat message', "disconnect member")
   });
-  socket.on('send file',function(data){
-     fs.writeFile('test/test.html',data,function(err){
-         if(err)throw err;
-         console.log("savefile")
-         io.emit('open file',data);
-     }) 
+  socket.on('send file', function (data) {
+    fs.writeFile('test/test.html', data, function (err) {
+      if (err) 
+        throw err;
+      console.log("savefile")
+      io.emit('open file', data);
+    })
   });
   fs.readFile('test/test.html', function (err, data) {
     if (err) 
